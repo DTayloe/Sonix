738df5301890dcd34347a3104eda444bb70b93c2
DTayloe <diomoidapple@gmail.com>
2026-02-08 16:47:28 -0500

    Dart SDK 3.7 compatibility

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\Users\\Daniel\\fvm\\versions\\3.29.0" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "G:\\Development\\Libraries\\sonix\\Sonix" PROJECT_DIR)
+
+set(FLUTTER_VERSION "2.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\Users\\Daniel\\fvm\\versions\\3.29.0"
+  "PROJECT_DIR=G:\\Development\\Libraries\\sonix\\Sonix"
+  "FLUTTER_ROOT=G:\\Users\\Daniel\\fvm\\versions\\3.29.0"
+  "FLUTTER_EPHEMERAL_DIR=G:\\Development\\Libraries\\sonix\\Sonix\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=G:\\Development\\Libraries\\sonix\\Sonix"
+  "FLUTTER_TARGET=lib/sonix.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=G:\\Development\\Libraries\\sonix\\Sonix\\.dart_tool\\package_config.json"
+)
